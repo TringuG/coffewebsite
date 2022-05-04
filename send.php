4cf0d22ed7ad6131721a3932d3ec59cc85d3cca4
TringuG <richardnalepka@gmail.com>
2022-05-04 10:05:31 +0000

    kontakt

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
-$to      = 'nobody@example.com';  // replace 'nobody@example.com' with your email.
+$to      = 'info@selficciono.com';  
 $subject = 'New query from '.$_POST["name"]." ".$_POST["email"];
 $message = strip_tags($_POST["message"], '<br>');
 $headers = 'From: '.strip_tags($_POST["email"]).'' . "\r\n" .
@@ -7,7 +7,7 @@ $headers = 'From: '.strip_tags($_POST["email"]).'' . "\r\n" .
 
 $success = mail($to, $subject, $message, $headers);
 if ($success) {
-  echo "Your message has been successfully sent.";
+  echo "Vaša správa bola uspešne odoslaná! :)";
   exit;
 }
 ?>
